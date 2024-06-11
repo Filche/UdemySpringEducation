114e43ec00b6eda760c35a448b8efa522a167898
Filche <filche_merigold@mail.ru>
2024-06-11 19:06:21 +1000

    init commit

---
+++ src/main/java/CompanyRepository.java
@@ -0,0 +1,11 @@
+public class CompanyRepository {
+    private final ConnectionPool connectionPool;
+
+    public CompanyRepository(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public static CompanyRepository of(ConnectionPool connectionPool){
+        return new CompanyRepository(connectionPool);
+    }
+}
