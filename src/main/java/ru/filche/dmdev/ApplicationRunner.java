651f6ac38ee8ac2f0e84afe82c9fec47a7ab4811
Filche <filche_merigold@mail.ru>
2024-07-04 13:24:02 +1000

    Spring Boot init

--- src/main/java/ru/filche/dmdev/ApplicationRunner.java
+++ src/main/java/ru/filche/dmdev/ApplicationRunner.java
@@ -1,30 +1,11 @@
 package ru.filche.dmdev;
 
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.filche.dmdev.spring.config.ApplicationConfiguration;
-import ru.filche.dmdev.spring.database.pool.ConnectionPool;
-import ru.filche.dmdev.spring.database.repository.CrudRepository;
-import ru.filche.dmdev.spring.service.CompanyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.Serializable;
+@SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web","prod");
-            context.refresh();
-            //      clazz -> String -> Map<String, Object>
-            ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            CompanyService companyService = context.getBean("companyService", CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
